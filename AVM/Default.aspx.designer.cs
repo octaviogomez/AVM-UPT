740d3f7df643679749c3da7594282f2b9e907e11
Octavio Gomez <octavio_vs@hotmail.com>
2018-06-12 22:35:09 -0500

    Correcion de login

--- AVM/Default.aspx.designer.cs
+++ AVM/Default.aspx.designer.cs
@@ -65,14 +65,5 @@ namespace AVM {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
-        
-        /// <summary>
-        /// Image1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
     }
 }

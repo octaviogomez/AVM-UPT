6dcc0c333f6b8c5c7f0a0cf850e8c0573362fd0f
Carlos <carlosortegariveros@gmail.com>
2018-07-11 13:10:43 -0500

    n

---
+++ AVM/Paginas/Usuario/Cuestionarios.aspx.designer.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace AVM.Paginas.Usuario {
+    
+    
+    public partial class Cuestionarios {
+        
+        /// <summary>
+        /// Control Cuestionario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::AVM.Controles.Usuario.Cuestionario Cuestionario;
+    }
+}
